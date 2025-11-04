c53f69ab5cd5b4030106d6ebab13eb6357e41ecb
tenth <tenthmascot@gmail.com>
2025-11-04 00:14:45 -0500

    set up Main.lean

--- EC2025.lean
+++ EC2025.lean
@@ -1,3 +1,5 @@
 -- This module serves as the root of the `EC2025` library.
 -- Import modules here that should be built as part of the library.
-import EC2025.Basic
+import EC2025.SolutionUtils
+import EC2025.ECUtils
+import EC2025.Quest01
--- Main.lean
+++ Main.lean
@@ -1,4 +1,12 @@
 import EC2025
 
-def main : IO Unit :=
-  IO.println s!"Hello, {hello}!"
+/-- A list of the main definitions of all quests. -/
+def mains := [
+  Quest01.main
+]
+
+def main : IO Unit := do
+  for (main, quest) in mains.zipIdx 1 do
+    .println s!"Quest {quest}"
+    main
+    if quest < mains.length then IO.println ""
